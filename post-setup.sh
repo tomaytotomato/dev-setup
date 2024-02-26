e4734686a6870adf6a5379b4402e49c20950f719
tomaytotomato <971697+tomaytotomato@users.noreply.github.com>
2024-02-26 15:26:06 +0000

    Updating script

--- post-setup.sh
+++ post-setup.sh
@@ -3,3 +3,8 @@
 # Update Hblock hosts
 hblock -H /etc/hosts.allow -F /etc/hosts.deny
 
+# Install Linuxbrew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/bruce/.bashrc
